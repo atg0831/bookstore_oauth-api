cbb3661d635a388362ce2fb561536cb15a337b8a
atg0831 <atg0831@naver.com>
2021-03-05 02:55:04 +0900

    Feat: oauth-mocking test

---
+++ src/repository/rest/users_repository_test.go
@@ -0,0 +1,27 @@
+package rest
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func TestLoginUserTimeoutFromApi(t *testing.T) {
+
+}
+func TestLoginUserInvalidErrorInterface(t *testing.T) {
+
+}
+func TestLoginUserInvalidLoginCredentials(t *testing.T) {
+
+}
+
+// func TestLoginUserInvalidErrorInterface(t *testing.T) {
+
+// }
+// func TestLoginUserInvalidErrorInterface(t *testing.T) {
+
+// }
